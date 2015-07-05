af79da1ea69a8a5c8079dc06037730efb5b5fc70
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-04 21:43:07 -0400

    tidying up

--- parser.rb
+++ parser.rb
@@ -1,11 +1,11 @@
 def to_html(markdown)
-  markdown.split("\n\n").map { |chunk| chunk_to_html chunk }.join("\n\n")
+  markdown.split("\n\n").map { |chunk| chunk_to_html(chunk) }.join("\n\n")
 end
 
 def chunk_to_html(chunk)
   if chunk.start_with? "#"
     hashes, text = chunk.split(" ", 2)
-    "<h#{hashes.length}>#{inner_text text}</h#{hashes.length}>"
+    "<h#{hashes.length}>#{inner_text(text)}</h#{hashes.length}>"
   else
     "<p>\n#{inner_text(chunk).lines.map { |line| " #{line.chomp}\n" }.join}</p>"
   end
@@ -19,8 +19,8 @@ end
 
 input_filename = ARGV[0]
 output_filename = ARGV[1]
-markdown = File.read input_filename
-html = to_html markdown
-File.write output_filename, html
+markdown = File.read(input_filename)
+html = to_html(markdown)
+File.write(output_filename, html)
 puts "Converted #{input_filename} (#{markdown.lines.count} lines) to #{output_filename} (#{html.lines.count} lines)"
 
